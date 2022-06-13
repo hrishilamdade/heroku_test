bf994cd66c54de424d500d956a428167f0bd0e34
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2022-06-13 10:41:51 +0530

    settings.py changes

--- heroku_test/settings.py
+++ heroku_test/settings.py
@@ -134,4 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATTIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
